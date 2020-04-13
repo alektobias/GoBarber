600082cc6fb97625373b22226d9209812d61c423
Alek Tobias <alektobias@gmail.com>
2020-04-13 17:51:14 -0300

    Deleted everything to star a new version

--- mobile/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import createRouter from './routes';
-import { useSelector } from 'react-redux';
-
-export default function src() {
-  const { signed } = useSelector(state => state.auth);
-  const Routes = createRouter(signed);
-  return <Routes />;
-}
