0ab2e44a0f2bf9de0a0045c01bb487d2164f8096
Alek Tobias <alektobias@gmail.com>
2020-05-09 17:57:55 -0300

    feat: minor improvements

--- Server/src/config/upload.ts
+++ Server/src/config/upload.ts
@@ -5,7 +5,8 @@ import crypto from 'crypto';
 const tmpPath = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: tmpPath,
+  tmpFolder: tmpPath,
+  uploadsFolder: path.resolve(tmpPath, 'uploads'),
   storage: multer.diskStorage({
     destination: tmpPath,
     filename(request, file, callback) {
