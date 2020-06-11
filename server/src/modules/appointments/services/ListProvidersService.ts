66bbc69513789abfb66ae4b112edf5e9fa4f433b
Alek Tobias <alektobias@gmail.com>
2020-06-11 20:01:07 -0300

    feat: serialization to provider list

--- server/src/modules/appointments/services/ListProvidersService.ts
+++ server/src/modules/appointments/services/ListProvidersService.ts
@@ -5,6 +5,7 @@ import User from '@modules/users/infra/typeorm/entities/User';
 
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -28,7 +29,10 @@ export default class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
